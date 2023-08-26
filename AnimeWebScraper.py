f78e127c3063da4f21a7a310355d2011570235da
ImaginaryTurtle <josiahgg@colostate.edu>
2023-08-26 04:51:27 -0600

    Created main project files, finished FolderNames.py folder name getter.

--- AnimeWebScraper.py
+++ AnimeWebScraper.py
@@ -1,1 +1,11 @@
+import FolderNames
+import Scraper
 
+
+
+if __name__ == "__main__":
+    if FolderNames.create_anime_list():
+        #Do Stuff
+        print("Success")
+    else:
+        exit()
---
+++ FolderNames.py
@@ -0,0 +1,23 @@
+import os
+
+def get_folder_names(directory):
+    folder_names = []
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            folder_names.append(item)
+    return folder_names
+
+def create_anime_list():
+    directory = input("Enter the directory path in Windows format: ")
+    if os.path.exists(directory):
+        folder_names = get_folder_names(directory)
+        if folder_names:
+            folder_names.pop(0)
+            return folder_names
+        else:
+            print("No folders found in the directory.")
+            return False
+    else:
+        print("Directory does not exist.")
+        return False
